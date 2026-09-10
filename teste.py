b2f0aa4f91285dd236aee1643f9b2dd28ff35d69
mKesquita <kaiquemn2009@gmail.com>
2026-09-10 09:54:31 -0300

    testando o terceiro commit

--- teste.py
+++ teste.py
@@ -1,7 +1,1 @@
-hobby = input("Qual seu hobby?")
-print(f"Seu hobby é {hobby}")
-
-if hobby == "Escutar musica Funk alta no meio da madrugada" or hobby == "dar grau alto de moto a noite":
-    print("Parabens, voce é um cocô")
-if hobby == "vibecoding com lofi":
-    print("você é um cara sossegado")
+print('Deu certo!!!!')
